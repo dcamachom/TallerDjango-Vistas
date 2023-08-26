218b4b98b505a72e0af2c22c37789ce7605afef3
dcamachom <you@example.com>
2023-08-26 11:06:32 -0500

    preLab

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": 'casoprueba',
+        "USER": 'u2',
+        "PASSWORD": '12345',
         "HOST": "localhost",
         "PORT": "",
     }
